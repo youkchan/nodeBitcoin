1d7736370b3fb729e1fd68a88fc97a112495ad91
Yuki Tanaka <yukitanaka@yuki-no-macbook-pro.local>
2018-08-16 21:28:33 +0900

    balance

--- node/ViewController.swift
+++ node/ViewController.swift
@@ -16,16 +16,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.blue
         // Do any additional setup after loading the view, typically from a nib.
+        
         let web3 = Web3.new(URL(string: "http://127.0.0.1:8545")!)
-        let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        
+        //convert test btc private key -> rsk private key and create address
         let rskprivkey = privKeyToRskFormat(btcPrivateKey: "cRAEv7ENBqKbiRJv83bEhRx5qPKRwQJqGts6pcTSbu73JB7Zr1fa")
         let priv:PlainKeystore = PlainKeystore.init(privateKey: rskprivkey)!
-        print(rskprivkey)
-        print(priv.addresses)
+        //print(rskprivkey)
+        //print(priv.addresses)
 
-        //print(text.count - 5)
-        //print(text[text.index(text.startIndex, offsetBy: 1)...text.index(text.startIndex, offsetBy: text.count - 5)])
-        ecverify()
+        balanceCheck(web3: web3!)
+        //ecverify(web3: web3!)
+        
+        
 //        let address = EthereumAddress("0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826") // regtest
 ////        let address = EthereumAddress("0x34684c06822bab1f22fc8777bebbb3341c76f15e") // testnet
 //        let web3rin = Web3.new(URL(string: "http://153.126.153.29:4444")!)
@@ -97,23 +100,72 @@ class ViewController: UIViewController {
         return privKeyBytes
     }
     
-    func ecverify()-> String? {
+    func balanceCheck(web3:web3)-> Bool? {
+        
+        let abi = "[{\"constant\":true,\"inputs\":[],\"name\":\"name\",\"outputs\":[{\"name\":\"\",\"type\":\"string\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_spender\",\"type\":\"address\"},{\"name\":\"_value\",\"type\":\"uint256\"}],\"name\":\"approve\",\"outputs\":[{\"name\":\"\",\"type\":\"bool\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"totalSupply\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_from\",\"type\":\"address\"},{\"name\":\"_to\",\"type\":\"address\"},{\"name\":\"_value\",\"type\":\"uint256\"}],\"name\":\"transferFrom\",\"outputs\":[{\"name\":\"\",\"type\":\"bool\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"decimals\",\"outputs\":[{\"name\":\"\",\"type\":\"uint8\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"_owner\",\"type\":\"address\"}],\"name\":\"balanceOf\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"symbol\",\"outputs\":[{\"name\":\"\",\"type\":\"string\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_to\",\"type\":\"address\"},{\"name\":\"_value\",\"type\":\"uint256\"}],\"name\":\"transfer\",\"outputs\":[{\"name\":\"\",\"type\":\"bool\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"_owner\",\"type\":\"address\"},{\"name\":\"_spender\",\"type\":\"address\"}],\"name\":\"allowance\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"calcAmount\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"totalDeposit\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"payable\":true,\"stateMutability\":\"payable\",\"type\":\"fallback\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"name\":\"_from\",\"type\":\"address\"},{\"indexed\":true,\"name\":\"_to\",\"type\":\"address\"},{\"indexed\":false,\"name\":\"_value\",\"type\":\"uint256\"}],\"name\":\"Transfer\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"name\":\"_owner\",\"type\":\"address\"},{\"indexed\":true,\"name\":\"_spender\",\"type\":\"address\"},{\"indexed\":false,\"name\":\"_value\",\"type\":\"uint256\"}],\"name\":\"Approval\",\"type\":\"event\"},{\"constant\":false,\"inputs\":[],\"name\":\"deposit\",\"outputs\":[],\"payable\":true,\"stateMutability\":\"payable\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"withdraw\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[],\"name\":\"mint\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]"
+        let contractAddress = EthereumAddress("0xe85bca1671d1bb34c5bf1b7db51cbe0ac92c131e")!
+        let contract = web3.contract(abi, at: contractAddress, abiVersion: 1)!
+
+        let address_string = "0x4F0C2aB902a8B28F21c585b1d2a6Bd603082deCc"
+        let address = EthereumAddress(address_string)
+        var options = Web3Options.defaultOptions()
+        var gasLimit: BigUInt? = BigUInt(5000000000000) // - default gas limit
+        var gasPrice: BigUInt? = BigUInt(5000000000000) // - default gas price, quite small
+        options.gasLimit = gasLimit
+        options.gasPrice = gasPrice
+        options.from = address
+        guard let ret = contract.method("balanceOf", parameters: [address_string] as [AnyObject], options: options)?.call(options: nil) else {return false }
+        //let balance = ret.value!["0"]
+        let balance_any = ret.value!["0"]
+        print(balance_any)
+        
+        //Any?から取り出せない
+        
+//        if (balance as! Int > 10) {
+//            return true
+//        }
+
+        
+        return false
+    }
+
+    
+    func deposit(web3:web3, value:BigUInt) {
+        let abi = "[{\"constant\":true,\"inputs\":[],\"name\":\"name\",\"outputs\":[{\"name\":\"\",\"type\":\"string\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_spender\",\"type\":\"address\"},{\"name\":\"_value\",\"type\":\"uint256\"}],\"name\":\"approve\",\"outputs\":[{\"name\":\"\",\"type\":\"bool\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"totalSupply\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_from\",\"type\":\"address\"},{\"name\":\"_to\",\"type\":\"address\"},{\"name\":\"_value\",\"type\":\"uint256\"}],\"name\":\"transferFrom\",\"outputs\":[{\"name\":\"\",\"type\":\"bool\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"decimals\",\"outputs\":[{\"name\":\"\",\"type\":\"uint8\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"_owner\",\"type\":\"address\"}],\"name\":\"balanceOf\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"symbol\",\"outputs\":[{\"name\":\"\",\"type\":\"string\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_to\",\"type\":\"address\"},{\"name\":\"_value\",\"type\":\"uint256\"}],\"name\":\"transfer\",\"outputs\":[{\"name\":\"\",\"type\":\"bool\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"_owner\",\"type\":\"address\"},{\"name\":\"_spender\",\"type\":\"address\"}],\"name\":\"allowance\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"calcAmount\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"totalDeposit\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"payable\":true,\"stateMutability\":\"payable\",\"type\":\"fallback\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"name\":\"_from\",\"type\":\"address\"},{\"indexed\":true,\"name\":\"_to\",\"type\":\"address\"},{\"indexed\":false,\"name\":\"_value\",\"type\":\"uint256\"}],\"name\":\"Transfer\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"name\":\"_owner\",\"type\":\"address\"},{\"indexed\":true,\"name\":\"_spender\",\"type\":\"address\"},{\"indexed\":false,\"name\":\"_value\",\"type\":\"uint256\"}],\"name\":\"Approval\",\"type\":\"event\"},{\"constant\":false,\"inputs\":[],\"name\":\"deposit\",\"outputs\":[],\"payable\":true,\"stateMutability\":\"payable\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"withdraw\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[],\"name\":\"mint\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]"
+        let contractAddress = EthereumAddress("0xe85bca1671d1bb34c5bf1b7db51cbe0ac92c131e")!
+        let contract = web3.contract(abi, at: contractAddress, abiVersion: 1)!
+        let address_string = "0x4F0C2aB902a8B28F21c585b1d2a6Bd603082deCc"
+        let address = EthereumAddress(address_string)
+        var options = Web3Options.defaultOptions()
+        var gasLimit: BigUInt? = BigUInt(5000000000000) // - default gas limit
+        var gasPrice: BigUInt? = BigUInt(5000000000000) // - default gas price, quite small
+        options.gasLimit = gasLimit
+        options.gasPrice = gasPrice
+        options.from = address
+        let priv:PlainKeystore = PlainKeystore.init(privateKey: "891e9fde727d3504777424054db1408c48840d282e12ebf633de5051acfc81dd")!
+        let keystoreManager = KeystoreManager.init([priv])
+        web3.addKeystoreManager(keystoreManager)
+        options.value = value
+        var intermediateSend = contract.method(options: options)!
+        let sendResult = intermediateSend.send(password: "")
+        print(sendResult)
+    }
+    
+    func ecverify(web3:web3)-> Bool? {
     
         let abi = "[{\"constant\":true,\"inputs\":[{\"name\":\"hash\",\"type\":\"bytes32\"},{\"name\":\"signature\",\"type\":\"bytes\"}],\"name\":\"ecrecovery\",\"outputs\":[{\"name\":\"sig_address\",\"type\":\"address\"}],\"payable\":false,\"stateMutability\":\"pure\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"hash\",\"type\":\"bytes32\"},{\"name\":\"sig\",\"type\":\"bytes\"},{\"name\":\"signer\",\"type\":\"address\"}],\"name\":\"ecverify\",\"outputs\":[{\"name\":\"\",\"type\":\"bool\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"}]"
         
         let address = EthereumAddress("0x9DB516CC86D6278ee43BD91Bf935C4845Dc2B2D3")
-        //let web3rin = Web3.new(URL(string: "http://153.126.153.29:4444")!)
-        let web3rin = Web3.new(URL(string: "http://127.0.0.1:8545")!)
-        let balanceResult = web3rin?.eth.getBalance(address: address!)
-        guard case .success(let balance)? = balanceResult else {return nil}
+        let balanceResult = web3.eth.getBalance(address: address!)
+        guard case .success(let balance) = balanceResult else {return nil}
         //print(balance)
         
-        let gasPriceResult = web3rin?.eth.getGasPrice()
-        guard case .success(let gasprice)? = gasPriceResult else {return nil}
+        let gasPriceResult = web3.eth.getGasPrice()
+        guard case .success(let gasprice) = gasPriceResult else {return nil}
         //print(gasprice)
         
         let contractAddress = EthereumAddress("0x14abdb322d9ebd9a9b0ebe597a09d34344ab9c46")!
-        let contract = web3rin?.contract(abi, at: contractAddress, abiVersion: 1)!
+        let contract = web3.contract(abi, at: contractAddress, abiVersion: 1)!
         var options = Web3Options.defaultOptions()
         var gasLimit: BigUInt? = BigUInt(5000000000000) // - default gas limit
         var gasPrice: BigUInt? = BigUInt(5000000000000) // - default gas price, quite small
@@ -124,11 +176,6 @@ class ViewController: UIViewController {
         
         let msg_seed = String(arc4random())
         let msg_hex:String = "0x" + msg_seed.sha3(.keccak256)
-        //print(msg_hex)
-        //msg_hex = "0x8fc1fedd35837f0fb69fb9e4223161c0ac1abe7813dd565a10034d27ff7fb68b"
-        //var msg_hex:String = "0x8fc1fedd35837f0fb69fb9e422316"
-        //print(msg_hex)
-//        let msg_hex:String = "0x8fc1fedd35837f0fb69fb9e4223161c0ac1abe7813dd565a10034d27ff7fb68b"
         let msg = Data(hex: msg_hex)
         let signerAddress = EthereumAddress("0x4F0C2aB902a8B28F21c585b1d2a6Bd603082deCc")
         let pk = Data(hex: "891e9fde727d3504777424054db1408c48840d282e12ebf633de5051acfc81dd")
@@ -140,7 +187,6 @@ class ViewController: UIViewController {
         var signature_string = ""
         let (compressedSignature, _) = SECP256K1.signForRecovery(hash: msg, privateKey: pk, useExtraEntropy: false)
         print(compressedSignature?.toHexString())
-        //↑これが動かせれば良さそうだが、、
         
         
         //let signRes = web3rin?.personal.signPersonalMessage(message: msg_hex.data(using: .utf8)!, from: signerAddress!, password: "")
@@ -187,11 +233,11 @@ class ViewController: UIViewController {
         
         //print(signature_string)
         //msg_hex = "0x8fc1fedd35837f0fb69fb9e4223161c0ac1abe7813dd565a10034d27ff7fb68b"
-        guard let ret = contract?.method("ecrecovery", parameters: [msg_hex, signature_string] as [AnyObject], options: options)?.call(options: nil) else {return nil }
+        guard let ret = contract.method("ecrecovery", parameters: [msg_hex, signature_string] as [AnyObject], options: options)?.call(options: nil) else {return nil }
         print(ret)
         //guard case .success(let string) = result else {return nil}
         //print(string)
-        return "test"
+        return false
     }
 
 
